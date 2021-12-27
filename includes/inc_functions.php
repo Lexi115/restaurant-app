83d11772048980e149a0ac3846ac1e4bdf9c2048
Lexi115 <keiteekei.sica@gmail.com>
2021-12-27 23:58:50 +0100

    #7 started working on booking system

--- includes/inc_functions.php
+++ includes/inc_functions.php
@@ -1,9 +1,23 @@
 <?php
     require 'inc_database.php';
     
+    function to_array($query_result) {
+        if ($query_result->num_rows > 0) {
+            $arr = array();
+
+            while ($row = $query_result->fetch_assoc()) {
+                array_push($arr, $row);
+            }
+
+            return $arr;
+        } else {
+            return false;
+        }
+    }
+
     /* 
      * ----------------------------------------------- *
-     * Funzioni database
+     * Funzioni accounts
      * ----------------------------------------------- *
      */
 
@@ -22,18 +36,7 @@
         OR `token_accesso`='%s' OR `cod_gruppo` LIKE '%s' LIMIT %s OFFSET %s;";
 
         $result = $conn->query(sprintf($sql, $columns, $username, $token, $group, $limit, $page));
-
-        if ($result->num_rows > 0) {
-            $arr = array();
-
-            while ($row = $result->fetch_assoc()) {
-                array_push($arr, $row);
-            }
-
-            return $arr;
-        } else {
-            return false;
-        }
+        return to_array($result);
     }
 
     function create_user($username, $password, $group) {
@@ -49,3 +52,71 @@
 
         return $conn->query(sprintf($sql, $username, $password_hash, $group, $token));
     }
+
+    function customer_exists($fiscal_code) {
+        global $conn;
+
+        $sql = "SELECT `cf` FROM `clienti` WHERE `cf` = '%s';";
+        return ($conn->query(sprintf($sql, $fiscal_code)))->num_rows > 0;
+    }
+
+    function create_customer($fiscal_code, $last_name, $first_name, $phone_number, $address) {
+        global $conn;
+
+        $fiscal_code = $conn->real_escape_string($fiscal_code);
+
+        if (!customer_exists($fiscal_code)) {
+            $last_name = $conn->real_escape_string($last_name);
+            $first_name = $conn->real_escape_string($first_name);
+            $phone_number = $conn->real_escape_string($phone_number);
+            $address = $conn->real_escape_string($address);
+
+            $sql = "INSERT INTO `clienti` (`cf`, `cognome`, `nome`, `telefono`, `indirizzo`) 
+            VALUES ('%s', '%s', '%s', '%s', '%s');";
+
+            return $conn->query(sprintf($sql, $fiscal_code, $last_name, $first_name, $phone_number, $address));
+        }
+
+        return true;
+    }
+
+    function create_reservation($customer, $date, $time, $number_of_people, $notes = '') {
+        global $conn;
+
+        $customer = $conn->real_escape_string($customer);
+        $date = $conn->real_escape_string($date) . ' ' . $conn->real_escape_string($time) . ':00';
+        $number_of_people = $conn->real_escape_string($number_of_people);
+        $notes = $conn->real_escape_string($notes);
+        $reservation_id = bin2hex(random_bytes(10));
+
+        $sql = "INSERT INTO `prenotazioni` (`cod_prenotazione`, `cf_cliente`, `n_persone`, `note_aggiuntive`, `status`) 
+        VALUES ('%s', '%s', '%s', '%s', 1);";
+        $conn->query(sprintf($sql, $reservation_id, $customer, $number_of_people, $notes));
+
+
+        $table = get_free_table($date);
+        if (!$table) {
+            return false;
+        }
+
+        $sql = "INSERT INTO `tavoliprenotati` (`cod_prenotazione`, `numero_tavolo`, `data`) 
+        VALUES ('%s', '%s', '%s');";
+        echo sprintf($sql, $reservation_id, $table['numero_tavolo'], $date) . '<br>';
+        $conn->query(sprintf($sql, $reservation_id, $table['numero_tavolo'], $date));
+
+    }
+
+    function get_free_table($date) {
+        global $conn;
+
+        $sql = "SELECT * FROM `tavoli` WHERE `numero_tavolo` NOT IN (SELECT `numero_tavolo` FROM `tavoliprenotati`);";
+        $result = $conn->query($sql);
+        $table_array = to_array($result);
+
+        if (!$table_array) 
+            return false;
+
+        return $table_array[rand(0, count($table_array) - 1)];
+    }
+
+ 
